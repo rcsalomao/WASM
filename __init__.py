e9177f2c1fd3278781e8156ef00a083c74d43792
rcsalomao <rafael.c.salomao@gmail.com>
2022-07-17 15:47:10 -0300

    update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .WASM.wasm import WASM, generate_RV_2_param, serial_system, parallel_system
+from .WASM import WASM, generate_RV_2_param, serial_system, parallel_system
 
 
 __all__ = ["WASM", "generate_RV_2_param", "serial_system", "parallel_system"]
